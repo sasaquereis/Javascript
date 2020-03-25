322db24d80875ffb87c787b8147b89a6c892be63
sasaquereis <62626751+sasaquereis@users.noreply.github.com>
2020-03-24 23:52:12 -0300

    Commit do exercício do Curso em video Aula13

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert('olá!!!')
+function carregar() {
+    var msg = window.document.getElementById("msg")
+    var img = window.document.getElementById("imagem")
+    var date = new Date()
+    var hora = date.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >=0 && hora < 12) {
+      //BOM DIA!
+      img.scr = 'manhã.jpg'
+    }else if (hora >= 12 && hora < 18) { 
+      //BOA TARDE!
+      img.src = 'tarde.jpg'
+    }else{
+      //BOA NOITE!
+      img.src = 'noite.jpg'
+    }
+}
