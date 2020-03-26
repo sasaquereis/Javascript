fa307d8510871c8d2f77c38c4eb1b46406aa29ff
sasaquereis <62626751+sasaquereis@users.noreply.github.com>
2020-03-25 23:17:31 -0300

    Commit Aula12

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-function carregar() {
-    var msg = window.document.getElementById("msg")
-    var img = window.document.getElementById("imagem")
-    var date = new Date()
-    var hora = date.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >=0 && hora < 12) {
-      //BOM DIA!
-      img.scr = 'manhã.jpg'
-      document.body.style.background = '#e2cd9f'
-    }else if (hora >= 12 && hora < 18) { 
-      //BOA TARDE!
-      img.src = 'tarde.jpg'
-      document.body.style.background = '#b9846f'
-    }else{
-      //BOA NOITE!
-      img.src = 'noite.jpg'
-      document.body.style.background = '#515154'
+function verificar() {
+  var data = new Date()
+  var ano = data.getFullYear()
+  var fano = document.getElementById('txtano')
+  var res = document.querySelector('div#res')
+  if(fano.value.length == 0 || fano.value > ano ) {
+    window.alert('[ERROR] Verivique os dados e tente novamente!')
+  }else {
+    var fsex = document.getElementsByName('redsex')
+    var idade = ano - Number(fano.value)
+    res.innerHTML = `idade calculada: ${idade}`
+    var genero = ''
+    if (fsex[0].checked) {
+      genero = 'Homem'
+    } else if (fsex[1].checked) {
+      genoro = 'Muleher'
     }
-}
+      res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+  }
+}  
