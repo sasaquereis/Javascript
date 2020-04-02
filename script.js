b4273149625806ef5790fb27aedabf1a86259dc1
sasaquereis <62626751+sasaquereis@users.noreply.github.com>
2020-04-01 22:22:03 -0300

    commit da tabuada do curso em video

--- script.js
+++ script.js
@@ -1,52 +1,19 @@
-function verificar() {
-  var data = new Date()
-  var ano = data.getFullYear()
-  var fano = document.getElementById('txtano')
-  var res = document.querySelector('div#res')
-  if(fano.value.length == 0 || fano.value > ano ) {
-    window.alert('[ERROR] Verivique os dados e tente novamente!')
+function tabuada() {
+  let num = document.getElementById('txtn')
+  let tab = document.getElementById('seltab')
+  if (num.value.length == 0) {
+    window.alert('Por favor, digite um número!')
   }else {
-    var fsex = document.getElementsByName('radsex')
-    var idade = ano - Number(fano.value)
-    var genero = ''
-    var img = document.createElement('img')
-    img.setAttribute('id', 'foto')
-    if (fsex[0].checked) {
-      genero = 'Homem'
-      if (idade >=0 && idade <= 10){
-        //Criança
-        img.setAttribute('src','foto-bebe-m.png')
-      }else if (idade < 21){
-        //Joven
-        img.setAttribute('src','foto-jovem-m.png')
-      }else if (idade < 50){
-        //Adulto
-        img.setAttribute('src','foto-adulto-m.png')
-      }else{
-        //Idoso
-        img.setAttribute('src','foto-idoso-m.png')
-      }
-        
-    } else if (fsex[1].checked) {
-      genero = 'Mulher'
-      if (idade >=0 && idade <= 10){
-        //Criança
-        img.setAttribute('src','foto-bebe-f.png')
-      }else if (idade < 21){
-        //joven
-        img.setAttribute('src','foto-jovem-f.png')
-      }else if (idade < 50){
-        //Adulto
-        img.setAttribute('src','foto-adulto-f.png')
-      }else{
-        //Idoso
-        img.setAttribute('src','foto-idoso-f.png')
-      }
-        
-      
+    let n = Number(num.value)
+    let c = 1 
+    tab.innerHTML = ''
+    while (c <= 10) {
+      let item = document.createElement('option')
+      item.text = `${n} x ${c} = ${n*c}`
+      tab.appendChild(item)
+      c++
     }
-    res.style.textAlign = 'center'
-    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
-    res.appendChild(img)
   }
-}  
+  
+
+}
